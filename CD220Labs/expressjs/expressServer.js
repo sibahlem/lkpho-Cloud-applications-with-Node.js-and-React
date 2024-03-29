885f2b9b225664f9918c7b163a65b01b29591edb
Sibahle <60863779+sibahlem@users.noreply.github.com>
2024-03-29 03:26:23 +0200

    Update expressServer.js

--- CD220Labs/expressjs/expressServer.js
+++ CD220Labs/expressjs/expressServer.js
@@ -20,6 +20,20 @@ app.get("/:name",(req,res)=>{
     res.send("Hello "+req.params.name)
 })
 
+/*Create a list with the names of the month.
+Add an end point in the code /fetchMonth/:num which will fetch a particular month from a list and return it to user.
+If the number is invalid, it should return appropriate error message.*/
+const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+
+app.get("/fetchMonth/:num",(req,res)=>{
+    let num = parseInt(req.params.num);
+    if(num <1 || num >12) {
+        res.send("Not a valid month number")
+    } else {
+        res.send(months[num-1])
+    }
+})
+
 app.listen(3333, () => {
     console.log(`Listening at http://localhost:3333`)
 })
