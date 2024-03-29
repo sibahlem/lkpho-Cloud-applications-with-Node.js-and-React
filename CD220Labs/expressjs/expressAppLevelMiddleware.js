09cca378718ab3ec126e92cdc6b1e51a11fd6ad0
Sibahle <60863779+sibahlem@users.noreply.github.com>
2024-03-29 03:09:36 +0200

    Update expressAppLevelMiddleware.js

--- CD220Labs/expressjs/expressAppLevelMiddleware.js
+++ CD220Labs/expressjs/expressAppLevelMiddleware.js
@@ -17,3 +17,8 @@ app.listen(3333, () => {
     console.log(`listening at http://localhost:3333`)
 });
 
+/* This server uses middleware for authentication.
+If the password is not pwd123 it will not allow the user to login.
+This server has just one end point and it takes password as query parameter.
+ the server has a middleware which filters each request to the server to see what the password is
+ and allows to proceed only when the password is pwd123*/
